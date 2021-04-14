567d6766c7e3da3e7a02608ead5848d4e181a2c5
Yizi-Yeh <g384357@gmail.com>
2021-04-14 23:51:42 +0800

    go-first-test

---
+++ hello.go
@@ -0,0 +1,14 @@
+// 封包 一定要叫做main
+package main
+
+import "fmt"
+
+// 主程式
+func main() {
+	fmt.Println("Hello Golang")
+}
+
+// 撰寫程式 > 建置 (build) > 建置程式
+// 建置： go build 程式檔案的名稱
+// 執行程式：：輸入執行檔的檔名
+// macOS 建制完，執行程式需要加上 ./ => ./hello => Hello Golang
